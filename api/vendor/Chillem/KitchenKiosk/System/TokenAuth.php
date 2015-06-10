d34b7e8a39f2516ebd932ebe3d41d05340b08e5b
Cody Erekson <cody@erekson.org>
2015-06-09 23:10:25 -0500

    Add authentication registration class

---
+++ api/vendor/Chillem/KitchenKiosk/System/TokenAuth.php
@@ -0,0 +1,16 @@
+<?php
+    // Control authentication to API system via JSON web tokens
+
+namespace KitchenKiosk\System;
+
+use Luracast\Restler\iAuthenticate;
+
+class TokenAuth implements iAuthenticate {
+
+    function __isAllowed(){
+
+    }
+
+}
+
+?>
