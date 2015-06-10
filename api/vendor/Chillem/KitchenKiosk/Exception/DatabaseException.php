6523a2a5b645a8139a34692a771779fa7c4550bf
Cody Erekson <cody@erekson.org>
2015-06-10 17:04:40 -0500

    Remove legacy 'level' parameter usage from DatabaseException calls

--- api/vendor/Chillem/KitchenKiosk/Exception/DatabaseException.php
+++ api/vendor/Chillem/KitchenKiosk/Exception/DatabaseException.php
@@ -6,7 +6,7 @@ namespace KitchenKiosk\Exception;
 class DatabaseException extends \Exception {
 
         // want to make sure we don't screw up existing exception rules
-        public function __construct($message, $level="30", $code = 0, Exception $previous = null) {
+        public function __construct($message, $code = 0, Exception $previous = null) {
                 parent::__construct($message, $code, $previous);
 
         }
