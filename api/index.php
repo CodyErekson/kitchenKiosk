b153f127070ad5d90c51c3f029e87bd4499433fa
Cody Erekson <cody@erekson.org>
2015-07-01 11:09:49 -0500

    Trying to integrate external DIC (either Pimple or PHP-DI) with Restler's internal DIC, Scope.

--- api/index.php
+++ api/index.php
@@ -1,13 +1,28 @@
 <?php
 
+    namespace KitchenKiosk;
+
+    use Luracast\Restler\Scope;
     use Luracast\Restler\Restler;
 
-    require_once __DIR__ . '/load.php';
+    require_once __DIR__.'/vendor/autoload.php';
+
+    $file = __DIR__ . "/config.json";
+
+    $main = new Main($file);
+
+    $c = $main->c;
+
+    Scope::register('KitchenKiosk\\Drone\\HoneywellDrone', function () use ($c) { 
+        return $c->get('KitchenKiosk\\Drone\\HoneywellDrone');
+        //return new KitchenKiosk\Drone\HoneywellDrone($c->get('config'), $c->get('logger'));
+    });
 
     $r = new Restler(true,true);
     $r->addAPIClass('Luracast\\Restler\\Resources');
     $r->addAPIClass('KitchenKiosk\\Utility\\Display','display');
     $r->addAPIClass('KitchenKiosk\\Drone\\HoneywellDrone','drone');
+    $r->addAPIClass('KitchenKiosk\\Output\\DataTable','data');
     $r->addAuthenticationClass('KitchenKiosk\\System\\TokenAuth');
     $r->handle();
 
